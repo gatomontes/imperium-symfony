69b951db066b4f7e0d092ce78e494b7d4300fb2b
pulga <34066936+gatomontes@users.noreply.github.com>
2026-08-15 18:28:21 -0400

    Unify activation on generic v0 occupants

--- src/Command/ActivateCommand.php
+++ src/Command/ActivateCommand.php
@@ -4,35 +4,69 @@ declare(strict_types=1);
 
 namespace App\Command;
 
-use App\Bootstrap\Launcher;
+use App\Imperium\Runtime\Bootstrap\V0ActivationService;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-#[AsCommand(name: 'imperium:activate', description: 'Validate and advance primordial bootstrap through CURIA_READY')]
+#[
+    AsCommand(
+        name: "imperium:activate",
+        description: "Install required generic v0 occupants and bring Imperium to CURIA_READY",
+    ),
+]
 final class ActivateCommand extends Command
 {
-    public function __construct(private readonly Launcher $launcher)
-    {
+    public function __construct(
+        private readonly V0ActivationService $activation,
+    ) {
         parent::__construct();
     }
 
     protected function configure(): void
     {
-        $this->addArgument('instance-id', InputArgument::OPTIONAL, 'Immutable instance identifier', 'imperium-local');
+        $this->addArgument(
+            "instance-id",
+            InputArgument::OPTIONAL,
+            "Immutable instance identifier",
+            "imperium-local",
+        )->addOption(
+            "prepare-upgrades",
+            null,
+            InputOption::VALUE_NONE,
+            "Prepare but do not start the ordered v0 upgrade plan",
+        );
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
+    protected function execute(
+        InputInterface $input,
+        OutputInterface $output,
+    ): int {
         try {
-            $record = $this->launcher->activate((string) $input->getArgument('instance-id'));
+            $result = $this->activation->activate(
+                (string) $input->getArgument("instance-id"),
+                (bool) $input->getOption("prepare-upgrades"),
+            );
         } catch (\Throwable $exception) {
-            $output->writeln('<error>REFUSED</error> '.$exception->getMessage());
+            $output->writeln(
+                "<error>REFUSED</error> " . $exception->getMessage(),
+            );
             return self::FAILURE;
         }
-        $output->writeln('<info>'.$record['state'].'</info> generation '.$record['generation']);
+        $record = $result["state"];
+        $output->writeln(
+            "<info>" .
+                $record["state"] .
+                "</info> generation " .
+                $record["generation"],
+        );
+        $output->writeln("Activation: OPERATOR_ROOT_V0");
+        $output->writeln(
+            "Upgrade program: " . $result["upgrade_program_status"],
+        );
         return self::SUCCESS;
     }
 }
