2a1f67f9df52c005b60a7fd28641bbbf80c86f59
pulga <34066936+gatomontes@users.noreply.github.com>
2026-08-27 06:32:06 -0400

    Bind Guildhall gateway to acceptance authority

--- src/Imperium/Runtime/Guildhall/GuildhallCognitionGateway.php
+++ src/Imperium/Runtime/Guildhall/GuildhallCognitionGateway.php
@@ -7,6 +7,7 @@ namespace App\Imperium\Runtime\Guildhall;
 interface GuildhallCognitionGateway
 {
     public function deliberate(
+        string $acceptanceId,
         array $missionPlan,
         array $commissionScope,
         array $occupancy,
